f38b4b1229304f21e447a116560ed33fa6391e3d
euginedube <44766665+euginedube@users.noreply.github.com>
2019-01-03 15:48:40 +0200

    Add files via upload

---
+++ Assignment11.1.R
@@ -0,0 +1,33 @@
+setwd= ("C:/Users/eugine.dube/Documents")
+getwd()
+mybank =read.csv("bank-full.csv" , header = TRUE ,sep = ";")
+View(mybank)
+summary(mybank)
+#a. Create a visual for representing missing values in the dataset.
+mybank[mybank=='unknown'] = NA
+#table(mybank$job)
+summary(is.na(mybank))
+install.packages('VIM')
+library(VIM)
+aggr_plot <- aggr(mybank, col=c('navyblue','red'), numbers=FALSE, sortVars=TRUE, labels=names(mybank),  ylab=c("Histogram of missing data","Pattern"))
+
+#b. Show a distribution of clients based on a Job.
+jobcount <-table(mybank$job)
+
+
+barplot(height = jobcount[order(jobcount,decreasing = TRUE)] ,col = 'Red')
+
+#c. Check whether is there any relation between Job and Marital Status?
+#install.packages("gmodels")
+#library(gmodels)
+ 
+library(ggplot2)
+ggplot(mybank,aes(mybank$marital,mybank$job)) + geom_jitter()
+#There  is a relationship between job and marital status
+
+#d. Check whether is there any association between Job and Education?
+ggplot(mybank,aes(mybank$education,mybank$job)) + geom_jitter()
+#There is a relationship between job and education
+
+
+
